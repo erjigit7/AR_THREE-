4966197af3c8c2e96906af191a43e894696e5da0
erjigit7 <87127172+erjigit7@users.noreply.github.com>
2024-09-11 14:00:40 +0600

    asd

--- index.js
+++ index.js
@@ -1,54 +1,90 @@
-window.onload = () => {
-    let testEntitiesAdded = false;
-    alert('If testing the lat/lon manual input on a mobile device, please turn off your GPS to avoid the real location being detected.');
-    const el = document.querySelector("[gps-new-camera]");
-    el.addEventListener("gps-camera-update-position", e => {
-        if(!testEntitiesAdded) {
-            alert(`Got first GPS position: lon ${e.detail.position.longitude} lat ${e.detail.position.latitude}`);
-            // Add four boxes to the north (red), south (yellow), west (blue)
-            // and east (red) of the initial GPS position
-            const properties = [{
-                    color: 'red',
-                    latDis: 0.001,
-                    lonDis: 0
-                },{
-                    color: 'yellow',
-                    latDis: -0.001,
-                    lonDis: 0
-                },{
-                    color: 'blue',
-                    latDis: 0,
-                    lonDis: -0.001
-                },{
-                    color: 'green',
-                    latDis: 0,
-                    lonDis: 0.001
-                }
-            ];
-            for(const prop of properties) {
-                const entity = document.createElement("a-box");
-                entity.setAttribute("scale", {
-                    x: 20, 
-                    y: 20,
-                    z: 20
-                });
-                entity.setAttribute('material', { color: prop.color } );
-                entity.setAttribute('gps-new-entity-place', {
-                    latitude: e.detail.position.latitude + prop.latDis,
-                    longitude: e.detail.position.longitude + prop.lonDis
-                });
-                
-                document.querySelector("a-scene").appendChild(entity);
+const loadPlaces = function (coords) {
+    // COMMENT FOLLOWING LINE IF YOU WANT TO USE STATIC DATA AND ADD COORDINATES IN THE FOLLOWING 'PLACES' ARRAY
+    const method = 'api';
+
+    const PLACES = [
+        {
+            name: "Your place name",
+            location: {
+                lat: 42.860898, // add here latitude if using static data
+                lng: 74.602232, // add here longitude if using static data
             }
-            testEntitiesAdded = true;
+        },
+    ];
+
+    if (method === 'api') {
+        return loadPlaceFromAPIs(coords);
+    }
+
+    return Promise.resolve(PLACES);
+};
+
+// getting places from REST APIs
+function loadPlaceFromAPIs(position) {
+    const params = {
+        radius: 300,    // search places not farther than this value (in meters)
+        clientId: 'VAPJQY5YWII2J501KXAFXWUODU4FAMG2FLMT4HHO03V5C2DI',
+        clientSecret: 'ELNE4XWXLWFZNJAQWEXUNIOYMDQ2WXLZEJN3ALGEBSJUX2GC',
+        version: '20300101',    // foursquare versioning, required but unuseful for this demo
+    };
+
+    // CORS Proxy to avoid CORS problems
+    const corsProxy = 'https://cors-anywhere.herokuapp.com/';
+
+    // Foursquare API
+    const endpoint = `${corsProxy}https://api.foursquare.com/v2/venues/search?intent=checkin
+        &ll=${position.latitude},${position.longitude}
+        &radius=${params.radius}
+        &client_id=${params.clientId}
+        &client_secret=${params.clientSecret}
+        &limit=15
+        &v=${params.version}`;
+    return fetch(endpoint)
+        .then((res) => {
+            return res.json()
+                .then((resp) => {
+                    return resp.response.venues;
+                })
+        })
+        .catch((err) => {
+            console.error('Error with places API', err);
+        })
+};
+
+
+window.onload = () => {
+    const scene = document.querySelector('a-scene');
+
+    // first get current user location
+    return navigator.geolocation.getCurrentPosition(function (position) {
+
+        // than use it to load from remote APIs some places nearby
+        loadPlaces(position.coords)
+            .then((places) => {
+                places.forEach((place) => {
+                    const latitude = place.location.lat;
+                    const longitude = place.location.lng;
+
+                    // add place name
+                    const text = document.createElement('a-link');
+                    text.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+                    text.setAttribute('title', place.name);
+                    text.setAttribute('href', 'http://www.example.com/');
+                    text.setAttribute('scale', '20 20 20');
+
+                    text.addEventListener('loaded', () => {
+                        window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+                    });
+
+                    scene.appendChild(text);
+                });
+            })
+    },
+        (err) => console.error('Error in retrieving position', err),
+        {
+            enableHighAccuracy: true,
+            maximumAge: 0,
+            timeout: 27000,
         }
-    });
-
-    document.getElementById("go").addEventListener("click", e=> {
-        const lat = document.getElementById('lat').value;
-        const lon = document.getElementById('lon').value;
-        const minacc = document.getElementById('minacc').value;
-
-        el.setAttribute('gps-new-camera', { simulateLatitude: lat, simulateLongitude: lon, positionMinAccuracy: minacc } );
-    });
+    );
 };
